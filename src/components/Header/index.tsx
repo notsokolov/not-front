a8822b58fd343acb7a95c7eb98fc8186e2451ef5
Alexey Sokolov <notalexeysokolov@gmail.com>
2021-06-06 14:18:07 +0300

    navLinks moved into NavLinks component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,38 +11,14 @@ import {
   Background,
 } from "./style";
 
+
+import { navLinks } from '~/components/NavLinks'
 interface Props {
   activeRoute: string;
   setExpanded: any;
 }
 
-const navLinks = [
-  {
-    activeRoute: "Home",
-    href: "/",
-    title: "Главная",
-  },
-  {
-    activeRoute: "About",
-    href: "/about",
-    title: "Эбаут",
-  },
-  {
-    activeRoute: "Resume",
-    href: "/resume",
-    title: "Резюме",
-  },
-  {
-    activeRoute: "Bookmarks",
-    href: "/bookmarks",
-    title: "Закладки",
-  },
-  {
-    activeRoute: "Blog",
-    href: "/blog",
-    title: "Блог",
-  },
-];
+
 
 const NavLinks = ({ activeRoute, setExpanded }: Props) => {
   return (
---
+++ src/components/NavLinks/index.ts
@@ -0,0 +1,27 @@
+export const navLinks = [
+    {
+      activeRoute: "Home",
+      href: "/",
+      title: "Главная",
+    },
+    {
+      activeRoute: "About",
+      href: "/about",
+      title: "Эбаут",
+    },
+    {
+      activeRoute: "Resume",
+      href: "/resume",
+      title: "Резюме",
+    },
+    {
+      activeRoute: "Bookmarks",
+      href: "/bookmarks",
+      title: "Закладки",
+    },
+    {
+      activeRoute: "Blog",
+      href: "/blog",
+      title: "Блог",
+    },
+  ];
